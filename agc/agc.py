140a21693bfc05c5cb466f0b20172c40f7905042
Bou Maouya <boumaouya@eisti.eu>
2021-01-10 14:15:00 +0100

    2eme etape

--- agc/agc.py
+++ agc/agc.py
@@ -19,11 +19,11 @@ import os
 import gzip
 import statistics
 from collections import Counter
-from operator import itemgetter
+#from operator import itemgetter
 
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
-import nwalign3 as nw
+#import nwalign3 as nw
 
 __author__ = "Maouya Bou"
 __copyright__ = "EISTI / CY Tech"
@@ -48,7 +48,6 @@ def isfile(path):
         raise argparse.ArgumentTypeError(msg)
     return path
 
-
 def get_arguments():
     """Retrieves the arguments of the program.
       Returns: An object that contains the arguments
@@ -57,7 +56,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True,
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
     parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
                         help="Minimum sequence length for dereplication")
@@ -72,9 +71,12 @@ def get_arguments():
     return parser.parse_args()
 
 def read_fasta(amplicon_file, minseqlen):
+    """ Prend deux arguments correspondant au fichier fasta ou fasta.gz et à la longueur minimale des
+    séquences et retourne un ​ générateur de séquences de longueur l >= ​ minseqlen ​ : ​ yield
+    sequence.
+    """
     if (isfile(amplicon_file)):
         with gzip.open(amplicon_file, "rt") as fpt:
-            #next_line = 0
             contig =''
             for apt in fpt:
                 if len(apt.strip())==0 or apt[0]=='>' :
@@ -86,15 +88,20 @@ def read_fasta(amplicon_file, minseqlen):
                     contig = contig + apt.strip()
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    """ Prend trois arguments correspondant au fichier fasta, la longueur minimale des séquences et
+    leur comptage minimum. Retourne un générateur des séquences uniques ayant une occurrence O>=​ mincount ainsi que leur
+    occurrence
+    """
     sequences = list(read_fasta(amplicon_file, minseqlen))
-
     liste = Counter(sequences).most_common()
-
     for sequence in liste:
         if sequence[1]>=mincount:
             yield sequence
-        
+
 def get_chunks(sequence, chunk_size):
+    """ Prend une séquence et une longueur de segment l: ​ chunk_size et retourne une liste de
+    sous-séquences de taille l non chevauchantes
+    """
     liste_segment = []
     cpt = 0
     for i in range(4):
@@ -106,27 +113,35 @@ def get_chunks(sequence, chunk_size):
 def get_unique(ids):
     return {}.fromkeys(ids).keys()
 
-
-def common(lst1, lst2): 
+def common(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 def cut_kmer(sequence, kmer_size):
-
+    """Prend une séquence et une longueur de séquence k et retourne un générateur de tous les mots
+    de longueur k présents dans cette séquence
+    """
     for i, chaine in enumerate(sequence):
-        if(len(sequence)-i >= kmer_size):  
+        if(len(sequence)-i >= kmer_size):
             seq = sequence[i:i+kmer_size]
             yield seq
 
 
 def get_identity(alignment_list):
+    """Prend un alignement (sous forme de liste) et calcule le pourcentage d’identité entre les deux
+    nb nucléotides identiques séquences selon la formule
+    """
     count=0
     for kpt in range(len(alignment_list[0])):
         if alignment_list[0][kpt] == alignment_list[1][kpt]: count=count+1
     id_= count *100 / len(alignment_list[0])
-
     return id_
 
 def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    """Prend un dictionnaire ayant pour clé un dictionnaire de kmer (vide au départ)
+    et pour valeur une liste d’identifiant des séquences dont ils proviennent.
+    Retourne un dictionnaire de kmer contenant les kmers uniques présents dans chaque séquence
+    pour une longueur de donnée de kmer
+    """
 
     kmer_list = list(cut_kmer(sequence, kmer_size))
 
@@ -139,6 +154,9 @@ def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
     return kmer_dict
 
 def search_mates(kmer_dict, sequence, kmer_size):
+    """Prend un dictionnaire ayant pour clé un index de kmer et pour valeur une liste d’identifiant
+    des séquences dont ils proviennent, une séquence et une longueur de kmer.
+    Retourne une liste de 8 séquences les plus similaires à la séquence d'entrée"""
 
     liste_ids = []
     liste_seq=[]
@@ -148,13 +166,18 @@ def search_mates(kmer_dict, sequence, kmer_size):
         if kmer in kmer_dict:
             for ipt in kmer_dict[kmer]:
                 liste_ids.append(ipt)
-    
+
     for apt in Counter(liste_ids).most_common(8):
         liste_seq.append(apt[0])
     return liste_seq
 
 def detect_chimera(perc_identity_matrix):
-
+    """Prend une matrice donnant par segment le taux d’identité entre la séquence candidate et deux
+    séquences parentes et retourne un booléen indiquant si la séquence candidate est une chimère.
+    Si l’écart type moyen des pourcentages est supérieur à ​ 5 et que 2 segments minimum
+    de notre séquence montrent une similarité différente à un des deux parents, nous
+    identifierons cette séquence comme chimérique.
+    """
     ecarts_types = []
     sequence1 = False
     sequence2 = False
@@ -167,8 +190,7 @@ def detect_chimera(perc_identity_matrix):
             sequence2 = True
         if statistics.mean(ecarts_types) > 5 and sequence1 and sequence2:
             return True
-        else:
-            return False
+        return False
 
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
     pass
@@ -177,13 +199,17 @@ def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, 
     pass
 
 def write_OTU(OTU_list, output_file):
+    """Prend une liste d’OTU et le chemin vers un fichier de sortie et affiche les OTU au format:
+    >OTU_{numéro partant de 1} occurrence:{nombre d’occurrence à la déréplication}
+    {séquence au format fasta}
+    """
     def fill(text, width=80):
         """Split text with a line return to respect fasta format"""
         return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
     # Ecrire dans le fichier
-    with open(output_file, "wt") as f:
+    with open(output_file, "wt") as fi:
         for k,contigs in enumerate(OTU_list):
-            f.write(f">OTU_{k+1} occurrence:{contigs[1]}\n" + fill(contigs[0], width=80) + "\n")
+            fi.write(f">OTU_{k+1} occurrence:{contigs[1]}\n" + fill(contigs[0], width=80) + "\n")
 
 #==============================================================
 # Main program
